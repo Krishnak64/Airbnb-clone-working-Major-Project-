10e1cf160d4823bbbec7bcf59e51c6141e85c259
krishnaStr <krir@jkhjd>
2025-10-26 12:34:54 +0530

    Updated Project File

--- controllers/listings.js
+++ controllers/listings.js
@@ -2,7 +2,7 @@
 
 const Listing = require("../models/listing");
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken = process.env.Map_TOKEN;
+const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 
